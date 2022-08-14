54f720cc75d821b9cf1a3022faee9e046b77d8ee
tubi55 <tubi55@nate.com>
2022-08-14 12:42:53 +0900

    활성화 패널 텍스트 모션

--- src/App.js
+++ src/App.js
@@ -3,6 +3,16 @@ import './scss/style.scss';
 import { useRef, useState, useEffect } from 'react';
 
 function App() {
+	const data = [
+		{ title: '111', subTitle: '111', pic: 'pic1.jpg' },
+		{ title: '222', subTitle: '222', pic: 'pic2.jpg' },
+		{ title: '333', subTitle: '333', pic: 'pic3.jpg' },
+		{ title: '444', subTitle: '444', pic: 'pic4.jpg' },
+		{ title: '555', subTitle: '555', pic: 'pic5.jpg' },
+		{ title: '666', subTitle: '666', pic: 'pic6.jpg' },
+		{ title: '777', subTitle: '777', pic: 'pic7.jpg' },
+		{ title: '888', subTitle: '888', pic: 'pic8.jpg' },
+	]
 	const path = process.env.PUBLIC_URL;
 	const len = 8;
 	const list = useRef(null);
@@ -41,14 +51,17 @@ function App() {
 	return (
 		<main>
 			<ul ref={list} className='list'>
-				{Array(len).fill().map((item, idx) => {
+				{data.map((item, idx) => {
 					let activeClass = '';
 					Active === idx && (activeClass = 'on')
 					return (
 						<li key={idx} className={activeClass} >
 							<div className='inner'>
-								<img src={`${path}/img/pic${idx + 1}.jpg`} alt={idx} />
-								<div className="txt">{idx + 1}</div>
+								<img src={`${path}/img/${item.pic}`} alt={idx} />
+								<div className="txtActive">
+									<div className="upper"><p>{item.title}</p></div>
+									<div className="lower"><p>{item.subTitle}</p></div>
+								</div>
 							</div>
 						</li>
 					)
